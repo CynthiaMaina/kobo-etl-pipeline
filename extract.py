d2ee289c4645211110f0b9bb1912f585a372eb20
Cynthia Maina <cynthiamaina588@gmail.com>
2026-07-27 22:12:30 +0300

    Added Paginated Kobo API Extractor

--- extract.py
+++ extract.py
@@ -8,24 +8,27 @@ from config import (
 )
 
 def extract_data():
-    #extract training data and return it as a pandas dataframe
+    #extract Kobo submissions and return them as a pandas dataframe
 
     url = f"{KOBO_BASE_URL}/api/v2/assets/{KOBO_ASSET_ID}/data/"
     headers = {
         "Authorization" : f"Token {KOBO_API_TOKEN}"
     }
     try:
-        response = requests.get(url, headers=headers, timeout=30)
-        response.raise_for_status()
+        all_results = []
+        while url:
+            response = requests.get(url, headers=headers, timeout=30)
+            response.raise_for_status()
+            data = response.json()
+            all_results.extend(data.get('results',[]))
+            url = data.get('next')
+        
     except requests.exceptions.RequestException as e:
         raise Exception(f"Error connecting to Kobo API: {e}")
 
-    print(f"Status code: {response.status_code}")
-
-    if response.status_code != 200:
-        raise Exception(f"Request failed : {response.text}")
+    print(f"Extracted: {len(all_results)} records")
     
-    data = response.json()
-    df = pd.json_normalize(data["results"])
+
+    df = pd.json_normalize(all_results)
 
     return df
